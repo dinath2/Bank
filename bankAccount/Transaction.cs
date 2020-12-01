39852281ea5e744032d5f181761c2bd350bd278f
dinath2 <65164585+dinath2@users.noreply.github.com>
2020-12-01 11:05:52 +0100

    Update Transaction.cs

--- bankAccount/Transaction.cs
+++ bankAccount/Transaction.cs
@@ -6,7 +6,9 @@ using System.Text;
     {
     /*Jobben til Transaction er å holde på metoder og detaljer relatert til det å overføre til andre kontoer*/
 
-        static void Transfer() { 
-        //transfer Balance on current AccountID to specified AccountID's Balance
-        }
+        static void Transfer() {
+        //hvilken konto skal dette til? skriv inn en konto
+        //sjekk saldoen. hvis den er høy nok, får du lov til å overføre
+        //saldo overføres
     }
+}
